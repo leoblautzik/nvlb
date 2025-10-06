f13bc0b28407048f247ae80351aa4a8885b9bbe3
leoblautzik <leoblautzik@gmail.com>
2025-10-05 22:38:40 -0300

    Configuracion estable (less is more)

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,9 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("gitsigns").setup({
+		local gitsigns = require("gitsigns")
+
+		gitsigns.setup({
 			signs = {
 				add = { text = "│" },
 				change = { text = "│" },
@@ -10,16 +12,22 @@ return {
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
-			current_line_blame = true, -- muestra autor y fecha en la línea actual
-			current_line_blame_opts = { delay = 300 },
+			signcolumn = true,
+			numhl = false,
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol",
+				delay = 500,
+				ignore_whitespace = false,
+			},
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
-				local map = function(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
+				local map = function(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
 				end
 
+				-- Navegación entre cambios
 				map("n", "]c", function()
 					if vim.wo.diff then
 						return "]c"
@@ -28,7 +36,7 @@ return {
 						gs.next_hunk()
 					end)
 					return "<Ignore>"
-				end, { expr = true })
+				end, "Siguiente cambio")
 
 				map("n", "[c", function()
 					if vim.wo.diff then
@@ -38,14 +46,16 @@ return {
 						gs.prev_hunk()
 					end)
 					return "<Ignore>"
-				end, { expr = true })
+				end, "Cambio anterior")
 
-				map("n", "<leader>hs", gs.stage_hunk)
-				map("n", "<leader>hr", gs.reset_hunk)
-				map("n", "<leader>hp", gs.preview_hunk)
+				-- Acciones
+				map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+				map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+				map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
 				map("n", "<leader>hb", function()
 					gs.blame_line({ full = true })
-				end)
+				end, "Blame línea")
+				map("n", "<leader>hd", gs.diffthis, "Diff actual")
 			end,
 		})
 	end,
