b2a817a0890af2a1aa1dca94cc0b1195aecf3c04
leoblautzik <leoblautzik@gmail.com>
2025-10-18 16:18:56 -0300

    Quito FixCursorHold para evitar que se mueva y cambie los modos

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -5,12 +5,9 @@ return {
     dependencies = {
       'nvim-neotest/nvim-nio',
       'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
+      --'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
-      {
-        'nvim-neotest/neotest-go',
-      },
-
+      'nvim-neotest/neotest-go',
       'nvim-neotest/neotest-python',
     },
     config = function()
