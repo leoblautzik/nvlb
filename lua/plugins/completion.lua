d8ea87799c17be08fe4c19310dd0286bb18223f6
leoblautzik <leoblautzik@gmail.com>
2025-10-26 23:21:31 -0300

    blink con integración highlight-colors

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,60 +1,46 @@
 return {
+  -- Motor de snippets
   {
-    'brenoprata10/nvim-highlight-colors',
+    'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    build = (not jit.os:find 'Windows') and 'make install_jsregexp',
     config = function()
-      require('nvim-highlight-colors').setup {}
+      require('luasnip.loaders.from_vscode').lazy_load()
     end,
   },
 
+  -- Autocompletado con blink
   {
     'saghen/blink.cmp',
     dependencies = {
+      'L3MON4D3/LuaSnip',
       'rafamadriz/friendly-snippets',
+      'brenoprata10/nvim-highlight-colors',
     },
     version = '1.*',
-
     opts = {
-      keymap = { preset = 'enter' },
+      keymap = {
+        preset = 'enter',
+        ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+      },
+      appearance = { nerd_font_variant = 'mono' },
+      completion = { documentation = { auto_show = false } },
+      sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
       signature = { enabled = true },
-      appearance = {
-        nerd_font_variant = 'mono',
-      },
-      completion = {
-        menu = {
-          draw = {
-            components = {
-              -- customize the drawing of kind icons
-              kind_icon = {
-                text = function(ctx)
-                  -- default kind icon
-                  local icon = ctx.kind_icon
-                  -- if LSP source, check for color derived from documentation
-                  if ctx.item.source_name == 'LSP' then
-                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                    if color_item and color_item.abbr ~= '' then
-                      icon = color_item.abbr
-                    end
-                  end
-                  return icon .. ctx.icon_gap
-                end,
-                highlight = function(ctx)
-                  -- default highlight group
-                  local highlight = 'BlinkCmpKind' .. ctx.kind
-                  -- if LSP source, check for color derived from documentation
-                  if ctx.item.source_name == 'LSP' then
-                    local color_item = require('nvim-highlight-colors').format(ctx.item.documentation, { kind = ctx.kind })
-                    if color_item and color_item.abbr_hl_group then
-                      highlight = color_item.abbr_hl_group
-                    end
-                  end
-                  return highlight
-                end,
-              },
-            },
-          },
-        },
-      },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
     },
     opts_extend = { 'sources.default' },
+  },
+
+  -- Resaltado de colores en completado
+  {
+    'brenoprata10/nvim-highlight-colors',
+    config = function()
+      require('nvim-highlight-colors').setup {
+        enable_named_colors = true,
+        integration = { blink_cmp = true }, -- integración con blink
+      }
+    end,
   },
 }
